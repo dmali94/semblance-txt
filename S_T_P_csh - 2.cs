f1852a7a6835d8e0b4fad10461b7016c23ece86f
Ali Najafi <zero9196113387@gmail.com>
2016-03-24 18:16:30 +0430

    add function for  count of evry word...

--- S_T_P_csh - 2.cs
+++ S_T_P_csh - 2.cs
@@ -7,44 +7,65 @@ using System.Threading.Tasks;
 namespace S_T_P_csh
 {
     /// <summary>
-     class listNode
+    /// .
+    ///
+    class w_c                    ///////for take each word repet how meny
     {
-        private string data;
-        
+        public int count;
+        public string word;
+
+    }
+    class listNode
+    {
+        private w_c data;
+
         public listNode next;
-        public listNode(string x)
+        public listNode(w_c x)
         {
             data = x;
             next = null;
         }
+
         public listNode()
         {
-           
         }
-        public string Data
+        public w_c Data
         {
             get { return data; }
             set { data = value; }
         }
-       
+
         public listNode Next
         {
             get { return next; }
             set { next = value; }
         }
-        
+
     }
 
     class linkList
     {
         private listNode frist;
+        public int count;
         public linkList()
-
         {
+            count = 0;
             frist = null;
         }
-        public void Add_frist(string x)
+        public string place(int n)
         {
+            listNode current = frist;
+            int i = 1;
+            while (current.Next != null && i < n)
+            {
+                current = current.Next;
+                i++;
+            }
+            return current.Data.word;
+        }
+        public void Add_frist(w_c x)
+        {
+
             listNode temp = new listNode(x);
 
             if (frist == null)
@@ -54,8 +75,9 @@ namespace S_T_P_csh
                 temp.Next = frist;
                 frist = temp;
             }
+            count++;
         }
-        public void Add_last(string x)
+        public void Add_last(w_c x)
         {
             listNode temp = new listNode();
             temp.Data = x;
@@ -71,31 +93,28 @@ namespace S_T_P_csh
                 current.Next = temp;
 
             }
-          
-
-
-
+            count++;
         }
-        public bool delelte_place_n(int place,ref string x)
+        public bool delelte_place_n(int place)
         {
             listNode p = frist;
             int counter = 1;
-            
-            while(p.next!=null&&counter<place-1)
+
+            while (p.next != null && counter < place - 1)
             {
                 counter++;
                 p = p.next;
             }
-            if(counter==1)
+            if (counter == 1)
             {
-                x = p.Data;
                 frist = frist.next;                         /////?p=p.next ?dont work and dont applaid on frist;
+                count--;
                 return true;                                ////if next changed that frisrt changed too.
             }
             if (p.next != null)
             {
-                x = p.next.Data;
                 p.next = p.next.next;                      /////?
+                count--;
                 return true;
 
             }
@@ -104,50 +123,51 @@ namespace S_T_P_csh
         }
         public listNode Frist
         {
-            get{return frist;}
+            get { return frist; }
         }
 
-       
-
         public void show()
+        {
+            listNode a = frist;
+            while (a != null)
             {
-                listNode a = frist;
-                while (a != null)
-                {
-                    Console.WriteLine(a.Data.ToString());
-                    a = a.Next;
-                }
+                Console.WriteLine(a.Data.word.ToString());
+                a = a.Next;
             }
-    
-        
- }
-    class w_c                    ///////for take each word repet how meny
-    {
-        public int count;
-        public string word;
+            Console.WriteLine(count.ToString());
+
+        }
+
 
     }
-    
+
+
 
     /// </summary>
     /// 
-   
+
     class Program
     {
-        
+
         static void Main(string[] args)
         {
-            //string s2 = "my name is ali najafi and my fatder name is\nyoosef.";
 
-            //linkList str ;
-            
-            //str = word_word(s2);
-            //str.show();
+            string s2 = "my name is \nali najafi.";
+
+            linkList str;
+            linkList str2;
+
+            str = word_word(s2);
+            str.show();
+            str2 = word_count(str);    //////bebin
+            Console.WriteLine("plase= " + str.place(1));
+            str2.show();
         }
 
         public static linkList word_word(string x)        /////for spilt sententes and put linklist
         {
             linkList str = new linkList();
+            w_c str_c;
             string[] star = x.Split('\n');
             string[] ozv;
 
@@ -155,11 +175,16 @@ namespace S_T_P_csh
             {
                 ozv = star[j].Split(' ');
                 for (int k = 0; k < ozv.Length; k++)
-                    str.Add_last(ozv[k]);
+                {
+                    str_c = new w_c();
+                    str_c.word = ozv[k];
+                    str_c.count = 1;
+                    str.Add_last(str_c);
+                }
             }
             return str;
         }
-        public float main_a(string m1, string m2)     ////main 2 ....
+        public float main_a(string m1, string m2)
         {
 
             linkList str1 = new linkList();
@@ -169,6 +194,31 @@ namespace S_T_P_csh
 
             return 100;
         }
-   
+        public static linkList word_count(linkList str)     //////for count of evry word...
+        {
+            int c = 1;
+            linkList a = new linkList();
+            w_c st;
+            for (int i = 1; i <= str.count; i++)
+            {
+                for (int j = i + 1; j < str.count; j++)
+                {
+                    if (str.place(i) == str.place(j))
+                    {
+                        str.delelte_place_n(j);
+                        c++;
+
+                    }
+                }
+                st = new w_c();
+                st.word = str.place(i);
+                st.count = c;
+                c = 1;
+                a.Add_last(st);
+
+            }
+            return a;
+        }
+
     }
 }
